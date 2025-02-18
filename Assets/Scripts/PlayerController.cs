266bde3aa450078f828e0cdedc194c65bed424da
Novotny_Filip <novof107@hsd.k12.or.us>
2025-02-18 11:13:56 -0800

    Lesson 1.3 checkpoint

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 20.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created 
     void Start()
     {
         
@@ -12,6 +13,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
